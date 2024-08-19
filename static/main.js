1b4824b2393811c31f4e10ca670369237476bf91
rvchk <115169534+k0m11@users.noreply.github.com>
2024-08-19 23:14:39 +0300

    Update main.js

--- static/main.js
+++ static/main.js
@@ -1,90 +1,108 @@
-let a = ""; // first number
-let b = ""; // second number
-let sign = ""; // operation sign
-let finish = false;
-let buttons = document.querySelector(".buttons-container")
+const buttons = document.querySelector(".buttons")
+const ClearAll = document.querySelector(".AC")
+const plusMinus = document.querySelector(".PM")
+const Percent = document.querySelector(".Percent")
+let result = document.querySelector(".result")
+let a = ""
+let b = ""
+let sign = ""
 
-const out = document.querySelector("h1")
-// initialize arrays
-const digit = ["0","1","2","3","4","5","6","7","8","9","0", "."];
-const action = ["-", "+", "X", "/", "%", "+/-"]
+const digits = ["0","1","2","3","4","5","6","7","8","9","."]
+const signs = ["+","-","/","X","+/-","AC"]
 
-// AC button function
-function clearAll() {
-    a = "";
-    b = "";
-    sign = "";
-    out.innerHTML = 0;
-}
+ClearAll.addEventListener("click", ()=> {
+    a = ""
+    b = ""
+    sign = ""
+    result.innerHTML = 0
+})
+
+plusMinus.addEventListener("click", ()=> {
+    if (sign == "") {
+        a = -a
+        result.innerHTML = a
+    }
+    else {
+        b = -b
+        result.innerHTML = b
+    }
+})
+
+Percent.addEventListener("click", ()=> {
+    if (sign == "") {
+        a = a/100
+        result.innerHTML = a
+    }
+    else {
+        b = b/100
+        result.innerHTML = b
+    }
+})
 
 buttons.addEventListener("click", (event)=> {
-    // isn't button pressed
     if(!event.target.classList.contains("button")) return;
+    if(event.target.classList.contains("AC")) return;
+    if(event.target.classList.contains("PM")) return;
 
-    // pressed clearAll button
-    else if(event.target.classList.contains("#ac")) return;
-
-    // getting pressed button
-    const key = event.target.innerHTML
-
-    // if button 0-9 or . was pressed
-    if (digit.includes(key)) {
-        if (b == "" && sign == "") {
+    let key = event.target.innerHTML
+    // for numbers
+    if(digits.includes(key)) {
+        if(b == "" && sign == "") {
             a += key
-            out.innerHTML = a
-            console.log(a)
+            result.innerHTML = a
         }
-        else if(a!== "" && b!== "" && finish) {
-            b = key;
-            finish = false;
-            out.innerHTML = b;
-        }
-        else {
-            b+= key;
-            out.innerHTML = b
-        }
-    }
-    // if button + - / X was pressed
-    else if (action.includes(key)) {     
-        if (key == "%"){
-            a = a/100
-            out.innerHTML = a
-        }
-        else if (key == "+/-"){
-            a = -a
-            out.innerHTML = a
-        }
-        else {
-        sign = key
-        out.innerHTML = sign
+        if(!sign == "") {
+            b += key
+            result.innerHTML = b
         }
     }
 
-    // if button = was pressed
-    else if(key == "=") {
-        if (b === "") b = a
+    // for signs
+    if(signs.includes(key)) {
+        switch(key) {
+            case "+":
+                sign = "+"
+                break
+            case "-":
+                sign = "-"
+                break
+            case "X":
+                sign = "X"
+                break
+            case "/":
+                sign = "/"
+                break
+        }
+        result.innerHTML = sign
+    }
+
+    if(key == "=") {
         switch(sign) {
             case "+":
-                a = (+(a)+(+b));
-                break;
+                a = (+a) + (+b)
+                result.innerHTML = a
+                break
             case "-":
-                a = (a-b);
-                break;
+                a = a - b
+                result.innerHTML = a
+                break
             case "X":
-                a = (a*b);
-                break;
+                a = a * b
+                result.innerHTML = a
+                break
             case "/":
-                if(b === "0"){
-                    out.innerHTML = "Error";
-                    a = "";
-                    b = "";
-                    sign = "";
-                    return;
+                if(b == 0) {
+                    a = ""
+                    result.innerHTML = "Error"
                 }
-                a = (a/b)
-                break;
+                else {
+                    a = a / b
+                    result.innerHTML = a
+                }
+                break
         }
-        finish = true
-        out.innerHTML = a 
+        b = ""
+        sign = ""
     }
+    console.log(a,sign,b)
 })
